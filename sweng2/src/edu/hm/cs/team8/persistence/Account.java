9d3cecd7d50c9ab7f5ca327ff6e4002bcca7a600
ahilmer <andi.hilmer@googlemail.com>
2013-04-22 19:48:53 +0200

    Datenmodell

---
+++ sweng2/src/edu/hm/cs/team8/persistence/Account.java
@@ -0,0 +1,24 @@
+package edu.hm.cs.team8.persistence;
+
+public class Account {
+
+	private String name;
+	
+	private Projekt project;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Projekt getProject() {
+		return project;
+	}
+
+	public void setProject(Projekt project) {
+		this.project = project;
+	}
+}
--- sweng2/src/edu/hm/cs/team8/persistence/Member.java
+++ sweng2/src/edu/hm/cs/team8/persistence/Member.java
@@ -2,4 +2,22 @@ package edu.hm.cs.team8.persistence;
 
 public class Member {
 
+	private long mId;
+
+	private String name;
+
+	private int level;
+
+	public int getLevel() {
+		return level;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public long getmId() {
+		return mId;
+	}
+
 }
