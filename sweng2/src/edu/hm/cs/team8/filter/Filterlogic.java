91fc0749856b1e7fd212d2e6606fd66ea1a5b206
ahilmer <andi.hilmer@googlemail.com>
2013-04-22 20:04:41 +0200

    Filterlogik

---
+++ sweng2/src/edu/hm/cs/team8/filter/Filterlogic.java
@@ -0,0 +1,7 @@
+package edu.hm.cs.team8.filter;
+
+public interface Filterlogic {
+
+	
+	public FilterResults filter(final Filter filter);
+}
---
+++ sweng2/src/edu/hm/cs/team8/filter/mock/FilterlogicMock.java
@@ -0,0 +1,15 @@
+package edu.hm.cs.team8.filter.mock;
+
+import edu.hm.cs.team8.filter.Filter;
+import edu.hm.cs.team8.filter.FilterResults;
+import edu.hm.cs.team8.filter.Filterlogic;
+
+public class FilterlogicMock implements Filterlogic {
+
+	@Override
+	public FilterResults filter(Filter filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
