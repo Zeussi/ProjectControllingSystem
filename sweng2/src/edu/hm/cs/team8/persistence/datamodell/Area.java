6f2f55bd37850f6c1502923b2e4c65457ad10a57
ahilmer <andi.hilmer@googlemail.com>
2013-04-22 19:51:29 +0200

    Neues Package

---
+++ sweng2/src/edu/hm/cs/team8/persistence/datamodell/Area.java
@@ -0,0 +1,16 @@
+package edu.hm.cs.team8.persistence;
+
+public class Area {
+
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ sweng2/src/edu/hm/cs/team8/persistence/datamodell/Projekt.java
@@ -0,0 +1,25 @@
+package edu.hm.cs.team8.persistence;
+
+public class Projekt {
+	
+	private String name;
+	
+	private Area area;
+
+	public Area getArea() {
+		return area;
+	}
+
+	public void setArea(Area area) {
+		this.area = area;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
