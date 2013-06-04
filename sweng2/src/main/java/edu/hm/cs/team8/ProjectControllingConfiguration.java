e7bb03c49b82cab55f761e82ace5312856c9a235
ndi <andi.hilmer@googlemail.com>
2013-06-04 20:02:15 +0200

    unnötiges entfernt

--- sweng2/src/main/java/edu/hm/cs/team8/ProjectControllingConfiguration.java
+++ sweng2/src/main/java/edu/hm/cs/team8/ProjectControllingConfiguration.java
@@ -3,20 +3,11 @@ package edu.hm.cs.team8;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.db.DatabaseConfiguration;
 
 public class ProjectControllingConfiguration extends Configuration {
-	@NotEmpty
-	@JsonProperty
-	private String template;
-
-	@NotEmpty
-	@JsonProperty
-	private String defaultName = "Stranger";
 
 	@Valid
 	@NotNull
@@ -26,13 +17,4 @@ public class ProjectControllingConfiguration extends Configuration {
 	public DatabaseConfiguration getDatabaseConfiguration() {
 		return database;
 	}
-
-	public String getTemplate() {
-		return template;
-	}
-
-	public String getDefaultName() {
-		return defaultName;
-	}
-
 }
