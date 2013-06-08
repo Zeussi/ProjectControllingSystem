52da79bcec70309fb0e53920927c4b441b3198f8
andi <andi.hilmer@googlemail.com>
2013-06-09 00:19:54 +0200

    statische seiten unter / services unter /service/

--- sweng2/src/main/java/edu/hm/cs/team8/ProjectControllingService.java
+++ sweng2/src/main/java/edu/hm/cs/team8/ProjectControllingService.java
@@ -4,10 +4,10 @@ import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
 
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 import com.yammer.dropwizard.jdbi.DBIFactory;
-import com.yammer.dropwizard.views.ViewBundle;
 
 import edu.hm.cs.team8.keyfiguresCalculator.IKeyFiguresCalculator;
 import edu.hm.cs.team8.keyfiguresCalculator.KeyFiguresCalculatorFactory;
@@ -17,7 +17,8 @@ import edu.hm.cs.team8.timetrackingmangement.ITimeTrackingMangement;
 import edu.hm.cs.team8.timetrackingmangement.TimeTrackingManagementFactory;
 import edu.hm.cs.team8.timetrackingmangement.ui.TimeTrackingResource;
 
-public class ProjectControllingService extends Service<ProjectControllingConfiguration> {
+public class ProjectControllingService extends
+		Service<ProjectControllingConfiguration> {
 	public static void main(String[] args) throws Exception {
 		new ProjectControllingService().run(args);
 	}
@@ -25,18 +26,23 @@ public class ProjectControllingService extends Service<ProjectControllingConfigu
 	@Override
 	public void initialize(Bootstrap<ProjectControllingConfiguration> bootstrap) {
 		bootstrap.setName("project-controlling-service");
-		bootstrap.addBundle(new ViewBundle());
+
+		bootstrap.addBundle(new AssetsBundle("/assets/", "/", "index.html"));
+
 	}
 
 	@Override
-	public void run(ProjectControllingConfiguration configuration, Environment environment)
-			throws ClassNotFoundException {
+	public void run(ProjectControllingConfiguration configuration,
+			Environment environment) throws ClassNotFoundException {
 
-		final DBI dbi = new DBIFactory().build(environment, configuration.getDatabaseConfiguration(), "hiveDB");
+		final DBI dbi = new DBIFactory().build(environment,
+				configuration.getDatabaseConfiguration(), "hiveDB");
 		final Handle handle = dbi.open();
 
-		final ITimeTrackingMangement management = TimeTrackingManagementFactory.makeTimeTrackingManagement(handle);
-		final IKeyFiguresCalculator calc = KeyFiguresCalculatorFactory.makeKeyFiguresCalculator(management);
+		final ITimeTrackingMangement management = TimeTrackingManagementFactory
+				.makeTimeTrackingManagement(handle);
+		final IKeyFiguresCalculator calc = KeyFiguresCalculatorFactory
+				.makeKeyFiguresCalculator(management);
 
 		environment.addResource(new TimeTrackingResource(management));
 		environment.addResource(new OnlyKeyFiguresResource(calc));
