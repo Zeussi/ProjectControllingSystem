1781a2c5a7ef4bd8cb12f35fc9b479048f09b3b8
ndi <andi.hilmer@googlemail.com>
2013-06-08 22:01:07 +0200

    Filterparser

---
+++ sweng2/src/main/java/edu/hm/cs/team8/keyfiguresCalculator/impl/FilterParser.java
@@ -0,0 +1,26 @@
+package edu.hm.cs.team8.keyfiguresCalculator.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.hm.cs.team8.filter.FilterFactory;
+import edu.hm.cs.team8.filter.IFilter;
+import edu.hm.cs.team8.keyfiguresCalculator.to.FilterTO;
+
+public class FilterParser {
+
+	public static IFilter[] parse(FilterTO[] filters) {
+		final Set<IFilter> result = new HashSet<>();
+		for (final FilterTO fto : filters) {
+			final IFilter filter = FilterFactory.makeFilter(fto.getName(), fto.getValue());
+
+			if (filter == null)
+				continue;
+
+			result.add(filter);
+
+		}
+		return result.toArray(new IFilter[0]);
+	}
+
+}
